d817174764d15220043f2b70612f7ea07de180a8
Max Görner <max@familie-goerner.eu>
2019-08-13 01:01:47 +0200

    Add layouts for Anne and rename layers

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -141,7 +141,7 @@ enum { MACRO_VERSION_INFO,
   *
   */
 
-enum { PRIMARY, FUNCTION }; // layers
+enum { LAYOUT_MAX, FUNCTION_MAX, LAYOUT_ANNE, FUNCTION_ANNE, NUMPAD_ANNE }; // layers
 
 
 /* This comment temporarily turns off astyle's indent enforcement
@@ -152,7 +152,7 @@ enum { PRIMARY, FUNCTION }; // layers
 KEYMAPS(
 
   // Edit this keymap to make a custom layout
-  [PRIMARY] = KEYMAP_STACKED
+  [LAYOUT_MAX] = KEYMAP_STACKED
   (Key_Escape, Key_1, Key_2, Key_3, Key_4, Key_5, Key_6,
    Key_Tab, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_CapsLock,   Key_A, Key_S, Key_D, Key_F, Key_G,
@@ -160,14 +160,14 @@ KEYMAPS(
    OSM(LeftControl), Key_Spacebar, OSM(LeftShift), OSM(LeftAlt),
    OSM(RightAlt),
 
-   OSL(FUNCTION),  Key_6, Key_7, Key_8,     Key_9,         Key_0,        Key_Minus,
+   OSL(FUNCTION_MAX),  Key_6, Key_7, Key_8,     Key_9,         Key_0,        Key_Minus,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_LeftBracket,
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_CapsLock,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     OSM(LeftGui),
    Key_CapsLock, OSM(RightShift), OSM(RightAlt), OSM(RightControl),
    OSM(RightAlt)),
 
-  [FUNCTION] =  KEYMAP_STACKED
+  [FUNCTION_MAX] =  KEYMAP_STACKED
   (___,      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_LEDEffectNext,
    Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
    Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
@@ -180,6 +180,51 @@ KEYMAPS(
                                Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
    Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
    ___, ___, Key_Enter, ___,
+   ___),
+
+  [LAYOUT_ANNE] = KEYMAP_STACKED
+  (Key_Escape,          Key_1, Key_2, Key_3, Key_4, Key_5, Key_LEDEffectNext,
+   Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
+   Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
+   Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
+   Key_LeftControl, Key_Backspace, Key_LeftGui, Key_LeftShift,
+   ShiftToLayer(FUNCTION_ANNE),
+
+   M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Backspace,
+   Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
+                  Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
+   Key_Spacebar,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     Key_Minus,
+   Key_RightShift, Key_LeftAlt, Key_W, Key_RightControl,
+   Key_RightAlt),
+
+  [FUNCTION_ANNE] =  KEYMAP_STACKED
+  (LockLayer(NUMPAD_ANNE),      Key_F1,           Key_F2,      Key_F3,     Key_F4,        Key_F5,           Key_CapsLock,
+   Key_Tab,  ___,              Key_mouseUp, ___,        Key_mouseBtnR, Key_mouseWarpEnd, Key_mouseWarpNE,
+   Key_Home, Key_mouseL,       Key_mouseDn, Key_mouseR, Key_mouseBtnL, Key_mouseWarpNW,
+   Key_End,  Key_PrintScreen,  Key_Insert,  ___,        Key_mouseBtnM, Key_mouseWarpSW,  Key_mouseWarpSE,
+   ___, Key_Delete, ___, ___,
+   ___,
+
+   Consumer_ScanPreviousTrack, Key_F6,                 Key_F7,                   Key_F8,                   Key_F9,          Key_F10,          Key_F11,
+   Consumer_PlaySlashPause,    Consumer_ScanNextTrack, Key_LeftCurlyBracket,     Key_RightCurlyBracket,    Key_LeftBracket, Key_RightBracket, Key_F12,
+                               Key_LeftArrow,          Key_DownArrow,            Key_UpArrow,              Key_RightArrow,  ___,              ___,
+   Key_PcApplication,          Consumer_Mute,          Consumer_VolumeDecrement, Consumer_VolumeIncrement, ___,             Key_Backslash,    Key_Pipe,
+   ___, ___, Key_Enter, ___,
+   ___),
+
+  [NUMPAD_ANNE] =  KEYMAP_STACKED
+  (___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___, ___, ___, ___,
+   ___, ___, ___, ___,
+   ___,
+
+   M(MACRO_VERSION_INFO),  ___, Key_7, Key_8,      Key_9,              Key_KeypadSubtract, ___,
+   ___,                    ___, Key_4, Key_5,      Key_6,              Key_KeypadAdd,      ___,
+                           ___, Key_1, Key_2,      Key_3,              Key_Equals,         ___,
+   ___,                    ___, Key_0, Key_Period, Key_KeypadMultiply, Key_KeypadDivide,   Key_Enter,
+   ___, ___, ___, ___,
    ___)
 ) // KEYMAPS(
 
