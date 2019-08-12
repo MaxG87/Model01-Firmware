7bc57f4e042e4dadb5160c7720c46253a459e84a
Max Görner <max@familie-goerner.eu>
2019-08-12 21:40:37 +0200

    Modifier auf rechte FN legen
    Ein Modifier wurde auf die rechte FN-Taste gelegt, um um die defekte
    Taste herumzuprogrammieren.

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -236,7 +236,7 @@ KEYMAPS(
                   Key_H, Key_J, Key_K,     Key_L,         Key_Semicolon, Key_Quote,
    Key_CapsLock,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     OSM(LeftGui),
    Key_CapsLock, OSM(RightShift), OSM(RightAlt), OSM(RightControl),
-   Key_PcApplication),
+   OSM(RightAlt)),
 
 
 #else
