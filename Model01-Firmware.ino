1e133e6d1bc243a3df5509d079f450f2d50a1660
Max Görner <max@familie-goerner.eu>
2019-08-15 23:45:53 +0200

    Enable OneShot for Anne's layout too

--- Model01-Firmware.ino
+++ Model01-Firmware.ino
@@ -135,14 +135,14 @@ KEYMAPS(
    Key_Backtick, Key_Q, Key_W, Key_E, Key_R, Key_T, Key_Tab,
    Key_PageUp,   Key_A, Key_S, Key_D, Key_F, Key_G,
    Key_PageDown, Key_Z, Key_X, Key_C, Key_V, Key_B, Key_Escape,
-   Key_LeftControl, Key_Spacebar, Key_LeftGui, Key_LeftShift,
+   OSM(LeftControl), Key_Spacebar, OSM(LeftGui), OSM(LeftShift),
    ShiftToLayer(FUNCTION_ANNE),
 
    M(MACRO_ANY),  Key_6, Key_7, Key_8,     Key_9,         Key_0,         Key_Minus,
    Key_Enter,     Key_Y, Key_U, Key_I,     Key_O,         Key_P,         Key_Equals,
                   Key_H, Key_J, Key_K,     Key_L,         XXX,           XXX,
-   Key_RightAlt,  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     XXX,
-   Key_RightShift, Key_LeftAlt, Key_W, Key_RightControl,
+   OSM(RightAlt),  Key_N, Key_M, Key_Comma, Key_Period,    Key_Slash,     XXX,
+   OSM(RightShift),OSM(LeftAlt), Key_W, OSM(RightControl),
    Key_Backspace),
 
   [FUNCTION_ANNE] =  KEYMAP_STACKED
